8114d4b0774f1d15cb5426a163518d1aac57608c
CamilaCamacho <ccamach5@jhu.edu>
2020-04-05 20:00:54 -0400

    Butterfly menu list create and show (not working tho)

--- app/src/main/java/com/example/mylevering/ButterflyMenuListAdapter.java
+++ app/src/main/java/com/example/mylevering/ButterflyMenuListAdapter.java
@@ -11,70 +11,55 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
+import java.util.ArrayList;
 
-public class ButterflyMenuListAdapter extends RecyclerView.Adapter<ButterflyMenuListAdapter.MyViewHolder> /*ArrayAdapter<ButterflyMenuOption>*/ {
+public class ButterflyMenuListAdapter extends RecyclerView.Adapter<ButterflyMenuListAdapter.MyViewHolder> {
 
-    private List<ButterflyMenuOption> ButterflyMenuList;
+    private ArrayList<ButterflyMenuOption> butterflyMenuList;
 
-    public ButterflyMenuListAdapter(List<ButterflyMenuOption> ButterflyMenuList) {
-        this.ButterflyMenuList = ButterflyMenuList;
+    public ButterflyMenuListAdapter(ArrayList<ButterflyMenuOption> butterflyMenuList) {
+        this.butterflyMenuList = butterflyMenuList;
     }
-
-    /*
-    private int resource;
-    public ButterflyMenuListAdapter(Context ctx, int res, List<ButterflyMenuOption> items) {
-        super(ctx, res, items);
-        resource = res;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LinearLayout menuListView;
-        ButterflyMenuOption bm = getItem(position);
-
-        if (convertView == null) {
-            menuListView = new LinearLayout(getContext());
-            String inflater = Context.LAYOUT_INFLATER_SERVICE;
-            LayoutInflater vi = (LayoutInflater) getContext().getSystemService(inflater);
-            vi.inflate(resource, menuListView, true);
-        } else {
-            menuListView = (LinearLayout) convertView;
-        }
-        TextView titleView = (TextView) menuListView.findViewById(R.id.);
-        TextView priceView = (TextView) menuListView.findViewById(R.id.);
-        TextView caloriesView = (TextView) menuListView.findViewById(R.id.);
-
-        titleView.setText(bm.getTitle());
-        priceView.setText(bm.getWhen());
-        caloriesView.setText(bm.getPaid()==1 ? "PAID" : "unpaid");
-
-        return menuListView;
-
-    } */
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        //LinearLayout linearLayout = LayoutInflater.from(parent).inflate(R.layout.butterfly_menu_item,parent,false);
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.butterfly_menu_item,parent,false);
+        //LinearLayout linearLayout = (LinearLayout) LayoutInflater.from(
+          //      parent.getContext()).inflate(R.layout.butterfly_menu_item,parent,false);
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.setMenuItemDetails(butterflyMenuList.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return butterflyMenuList.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        LinearLayout butterflyMenuItem;
-        public MyViewHolder(@NonNull LinearLayout itemView) {
+        //LinearLayout butterflyMenuItem;
+        TextView title;
+        TextView price;
+        TextView calories;
+
+        private void setMenuItemDetails(ButterflyMenuOption menuOption) {
+            title.setText(menuOption.getTitle());
+            price.setText(menuOption.getPrice());
+            calories.setText(menuOption.getCalories());
+        }
+
+        public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            butterflyMenuItem = itemView;
+            //butterflyMenuItem = itemView.findViewById(R.id.menu_item);
+            title = itemView.findViewById(R.id.menu_item_title);
+            price = itemView.findViewById(R.id.menu_item_price);
+            calories = itemView.findViewById(R.id.menu_item_calories);
+
         }
     }
 }
--- app/src/main/java/com/example/mylevering/ButterflyOrder.java
+++ app/src/main/java/com/example/mylevering/ButterflyOrder.java
@@ -7,10 +7,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ButterflyOrder extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<ButterflyMenuOption> butterflyMenuList;
+
+    //
+    private ButterflyMenuListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +26,16 @@ public class ButterflyOrder extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.menu_list);
         layoutManager = new LinearLayoutManager(this);
+
+        //list
+        butterflyMenuList = new ArrayList<ButterflyMenuOption>();
+        ButterflyMenuOption menuItem1 = new ButterflyMenuOption("Title1", "Price1", "250 Calories");
+        butterflyMenuList.add(menuItem1);
+        ButterflyMenuOption menuItem2 = new ButterflyMenuOption("Title2", "Price2", "250 Calories");
+        butterflyMenuList.add(menuItem2);
+        //list
         recyclerView.setLayoutManager(layoutManager);
-
-
+        adapter = new ButterflyMenuListAdapter(butterflyMenuList);
+        recyclerView.setHasFixedSize(true);
     }
 }
