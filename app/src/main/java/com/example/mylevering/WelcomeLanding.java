bb6bd7f99685b53f94226bf8b7569d0d05a571cb
soo99 <skim403@jhu.edu>
2020-04-12 18:45:23 +0900

    created sign up activity

--- app/src/main/java/com/example/mylevering/WelcomeLanding.java
+++ app/src/main/java/com/example/mylevering/WelcomeLanding.java
@@ -2,7 +2,10 @@ package com.example.mylevering;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.mylevering.ui.login.LoginActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void onPressLogIn() {
+        Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void onPressSignUp(Intent intent) {
+
     }
 }
