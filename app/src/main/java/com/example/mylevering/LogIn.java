f2fc13e61ec166e9e8839deab9c954034c60a9d2
soo99 <skim403@jhu.edu>
2020-04-14 20:40:15 +0900

    added code to landing activity to maintain user login status even after leaving app

--- app/src/main/java/com/example/mylevering/LogIn.java
+++ app/src/main/java/com/example/mylevering/LogIn.java
@@ -5,21 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class LogIn extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LogIn extends AppCompatActivity implements View.OnClickListener {
 
     private EditText username;
     private EditText pass;
+    private Button login;
+
+    private DatabaseReference dbref;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        dbref = FirebaseDatabase.getInstance().getReference();
+        auth = FirebaseAuth.getInstance();
+
         username = findViewById(R.id.username);
         pass = findViewById(R.id.pass);
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(this);
     }
 
     public void completeLogIn(View v) {
@@ -43,5 +58,10 @@ public class LogIn extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- app/src/main/java/com/example/mylevering/WelcomeLanding.java
+++ app/src/main/java/com/example/mylevering/WelcomeLanding.java
@@ -6,14 +6,60 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class WelcomeLanding extends AppCompatActivity {
 
     public static final int LOG_IN = 0;
+
+    private FirebaseAuth auth;
+    private DatabaseReference dbref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        auth = FirebaseAuth.getInstance();
+        dbref = FirebaseDatabase.getInstance().getReference();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Check auth on Activity start
+        if (auth.getCurrentUser() != null) {
+            onAuthSuccess(auth.getCurrentUser());
+        }
+    }
+
+    private void onAuthSuccess(FirebaseUser user) {
+        String username = usernameFromEmail(user.getEmail());
+
+        // Write new user
+        writeNewUser(user.getUid(), username, user.getEmail());
+
+        // Go to MainActivity
+        startActivity(new Intent(WelcomeLanding.this, MainActivity.class));
+        finish();
+    }
+
+    private String usernameFromEmail(String email) {
+        if (email.contains("@")) {
+            return email.split("@")[0];
+        } else {
+            return email;
+        }
+    }
+
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+        dbref.child("users").child(userId).setValue(user);
     }
 
     public void launchLogIn(View v) {
