2a4570d14d2fb82483cb0414c86e5ff10efd947d
soo99 <skim403@jhu.edu>
2020-04-14 17:24:02 +0900

    added id field to Order class to uniquely identify each order placed

--- app/src/main/java/com/example/mylevering/Order.java
+++ app/src/main/java/com/example/mylevering/Order.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public class Order implements Serializable {
 
+    private static long nextId = 0;
+
     private String title;
     private String description;
     private String price;
@@ -12,37 +14,51 @@ public class Order implements Serializable {
     private int status;
     private boolean favorite;
     private String type;
+    private String id;
 
     public Order(MenuOption menuOption, String from, String schedTime, String method, int stat, boolean fav) {
-        title = menuOption.getTitle();
-        type = from;
-        description = menuOption.getDescription();
-        price = menuOption.getPrice();
-        scheduledTime = schedTime;
-        paymentMethod = method;
-        status = stat;
-        favorite = fav;
+        this.title = menuOption.getTitle();
+        this.description = menuOption.getDescription();
+        this.price = menuOption.getPrice();
+        this.scheduledTime = schedTime;
+        this.paymentMethod = method;
+        this.status = stat;
+        this.favorite = fav;
+        this.type = from;
+        this.id = String.format("o-%04d", ++nextId);
     }
 
     public String getTitle() {
-        return title;
+        return this.title;
     }
 
     public String getDescription() {
-        return description;
+        return this.description;
     }
 
-    public String getPrice() {
-        return price;
-    }
+    public String getPrice() { return this.price; }
 
     public String getScheduledTime() {
-        return scheduledTime;
+        return this.scheduledTime;
+    }
+
+    public String getPaymentMethod() {
+        return this.paymentMethod;
     }
 
     public int getStatus() {
-        return status;
+        return this.status;
     }
+
+    public boolean isFavorite() {
+        return this.favorite;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getId() { return this.id; }
 
     public void setTitle(String title) {
         this.title = title;
@@ -60,28 +76,16 @@ public class Order implements Serializable {
         this.scheduledTime = scheduledTime;
     }
 
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getPaymentMethod() {
-        return paymentMethod;
-    }
-
     public void setPaymentMethod(String paymentMethod) {
         this.paymentMethod = paymentMethod;
     }
 
-    public boolean isFavorite() {
-        return favorite;
+    public void setStatus(int status) {
+        this.status = status;
     }
 
     public void setFavorite(boolean favorite) {
         this.favorite = favorite;
-    }
-
-    public String getType() {
-        return type;
     }
 
     public void setType(String type) {
