3ed45cdc028d3115529f4a30efb34760a8931ce2
soo99 <skim403@jhu.edu>
2020-04-14 18:34:19 +0900

    added dbase reference to past orders frag

--- app/src/main/java/com/example/mylevering/PastOrdersFrag.java
+++ app/src/main/java/com/example/mylevering/PastOrdersFrag.java
@@ -7,7 +7,26 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+
 public class PastOrdersFrag extends Fragment {
+
+    private FirebaseDatabase dbase;
+    private DatabaseReference dbref;
+    private List<Order> pOrders;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        dbase = FirebaseDatabase.getInstance();
+        dbref = dbase.getReference();
+
+        //TODO(jack): create listview/recyclerview and populate with past orders from database
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -16,6 +35,7 @@ public class PastOrdersFrag extends Fragment {
         if (main != null) {
             main.setTitle("Past Orders");
         }
+
         return inflater.inflate(R.layout.fragment_past_orders, container, false);
     }
 }
