c3f0aa4426dbb7f3191108d83c18f1137ab29fc7
ggivelb <ggivelb@gmail.com>
2020-04-20 20:52:36 -0400

    Updated butterfly menu option to have a set of dietary restrictions.

--- app/src/main/java/com/example/mylevering/ButterflyMenuOption.java
+++ app/src/main/java/com/example/mylevering/ButterflyMenuOption.java
@@ -4,15 +4,17 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 import java.io.Serializable;
+import java.util.Set;
 
 public class ButterflyMenuOption extends MenuOption implements Serializable {
 
     private String calories;
     private boolean available;
+    private Set<String> restrictions;
     public ButterflyMenuOption(){}
 
-    public ButterflyMenuOption(String t, String p, String c, String d, Boolean a) {
-        this.setAll(t, p, c, d, a);
+    public ButterflyMenuOption(String t, String p, String c, String d, Set<String> r, Boolean a) {
+        this.setAll(t, p, c, d, r, a);
     }
 
 /*    protected ButterflyMenuOption(Parcel in) {
@@ -34,18 +36,20 @@ public class ButterflyMenuOption extends MenuOption implements Serializable {
         }
     };*/
 
-    private void setAll(String title, String price, String calories, String description, boolean available){
+    private void setAll(String title, String price, String calories, String description, Set<String> restrictions, boolean available){
         this.title = title;
         this.price = price;
         this.calories = calories;
         this.description = description;
+        this.restrictions = restrictions;
         this.available = available;
     }
 
     public String getTitle() { return title; }
     public String getPrice() { return price; }
     public String getCalories() { return calories; }
-    public String getDescription() { return description; }
+    public String getDescription() { return description + "Contains: " + restrictions.toString().substring(1, restrictions.toString().length() - 1); }
+    public Set<String> getRestrictions() { return restrictions; }
     public boolean isAvailable() { return available; }
     public void setCalories(String c) {
         calories = c;
--- app/src/main/java/com/example/mylevering/ButterflyOrder.java
+++ app/src/main/java/com/example/mylevering/ButterflyOrder.java
@@ -12,6 +12,7 @@ import android.os.Parcelable;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 public class ButterflyOrder extends AppCompatActivity implements ButterflyMenuListAdapter.OnMenuItemListener {
@@ -29,31 +30,32 @@ public class ButterflyOrder extends AppCompatActivity implements ButterflyMenuLi
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
+        HashSet<String> meatRestrictions = new HashSet<>();
+        meatRestrictions.add("Meat");
+        meatRestrictions.add("Gluten");
+        meatRestrictions.add("Dairy");
+        HashSet<String> vegRestrictions = new HashSet<>();
+        vegRestrictions.add("Gluten");
+        vegRestrictions.add("Dairy");
         // Create list and add stuff to it
         butterflyMenuList = new ArrayList<ButterflyMenuOption>();
         ButterflyMenuOption tacoChicken = new ButterflyMenuOption("Chicken Taco", "$6.99", "350",
-                "Corn tortilla, marinated pulled chicken, black beans, shredded mozzarella, cilantro, lime juice, pico de gallo\n" +
-                "Contains: Meat", true);
+                "Corn tortilla, marinated pulled chicken, black beans, shredded mozzarella, cilantro, lime juice, pico de gallo\n", meatRestrictions, true);
         butterflyMenuList.add(tacoChicken);
         ButterflyMenuOption tacoVeggie = new ButterflyMenuOption("Veggie Taco", "$5.99", "300",
-                "Flour tortilla, bell peppers, black beans, shredded mozzarella, cilantro, lime juice, pico de gallo\n" +
-                        "Contains: Gluten", true);
+                "Flour tortilla, bell peppers, black beans, shredded mozzarella, cilantro, lime juice, pico de gallo\n", vegRestrictions, true);
         butterflyMenuList.add(tacoVeggie);
         ButterflyMenuOption tacoPork = new ButterflyMenuOption("Pork Taco", "$7.99", "400",
-                "Flour tortilla, marinated pulled pork, black beans, shredded mozzarella, cilantro, lime juice, pico de gallo\n" +
-                "Contains: Meat, Gluten", true);
+                "Flour tortilla, marinated pulled pork, black beans, shredded mozzarella, cilantro, lime juice, pico de gallo\n", meatRestrictions, true);
         butterflyMenuList.add(tacoPork);
         ButterflyMenuOption tortaChicken = new ButterflyMenuOption("Chicken Torta", "$6.99", "350",
-                "Bread, seasoned pulled chicken, black beans, avocado, pico de gallo\n" +
-                        "Contains: Meat, Gluten", false);
+                "Bread, seasoned pulled chicken, black beans, avocado, pico de gallo\n", meatRestrictions, false);
         butterflyMenuList.add(tortaChicken);
         ButterflyMenuOption tortaVeggie = new ButterflyMenuOption("Veggie Torta", "$5.99", "300",
-                "Bread, seasoned bell peppers, black beans, avocado, pico de gallo\n" +
-                "Contains: Gluten", true);
+                "Bread, seasoned bell peppers, black beans, avocado, pico de gallo\n", vegRestrictions, true);
         butterflyMenuList.add(tortaVeggie);
         ButterflyMenuOption tortaPork = new ButterflyMenuOption("Pork Torta", "$7.99", "400",
-                "Bread, marinated pulled pork, black beans, avocado, pico de gallo\n" +
-                "Contains: Meat, Gluten", true);
+                "Bread, marinated pulled pork, black beans, avocado, pico de gallo\n", meatRestrictions, true);
         butterflyMenuList.add(tortaPork);
 
         adapter = new ButterflyMenuListAdapter(butterflyMenuList, this, getApplicationContext());
