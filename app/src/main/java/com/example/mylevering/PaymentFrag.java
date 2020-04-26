7891362f957b73d1e6f13aaab894117ebe15a8de
Camila <camila10298@gmail.com>
2020-04-26 02:01:39 -0400

    Simple, non-list payment method page

--- app/src/main/java/com/example/mylevering/PaymentFrag.java
+++ app/src/main/java/com/example/mylevering/PaymentFrag.java
@@ -13,27 +13,14 @@ import java.util.ArrayList;
 
 public class PaymentFrag extends Fragment {
 
-    private RecyclerView recyclerView;
-    private RecyclerView.LayoutManager layoutManager;
-    private ArrayList<PaymentMethod> paymentMethodList;
-    private PaymentMethodListAdapter adapter;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         MainActivity main = (MainActivity) getActivity();
         if (main != null) {
             main.setTitle("Payment");
-            main.setContentView(R.layout.fragment_payment);
-            recyclerView = main.findViewById(R.id.payment_list);
-            layoutManager = new LinearLayoutManager(main);
         }
-        recyclerView.setLayoutManager(layoutManager);
 
-        paymentMethodList = new ArrayList<PaymentMethod>();
-        PaymentMethod jCard = new PaymentMethod("JCard", "$275", "14 Meals Per Week");
-
-        //return inflater.inflate(R.layout.fragment_payment, container, false);
         return inflater.inflate(R.layout.fragment_payment, container, false);
     }
 }
